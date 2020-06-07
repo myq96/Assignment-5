55e9e0f65c3fb7efca4aca33aa34df18785531db
myq96 <myq1996@live.com>
2020-06-07 17:12:26 +0500

    Assigment 5 Js is ready

--- javascript/chap5.js
+++ javascript/chap5.js
@@ -32,7 +32,25 @@ document.write ("The remainder is: " + (++initial % 3) + "<br><br>");
 var cost = 600;
 var ticket = 5;
 
-document.write ("Total cost to buy " + ticket + " tickets to a movie is " + (cost * ticket) + "PKR" )
+document.write ("Total cost to buy " + ticket + " tickets to a movie is " + (cost * ticket) + "PKR" + "<br><br>")
+
+
+// Question 5 
+var table = 4
+document.write ("Table of " + table + "<br>");
+
+document.write (table + "x1=" + (table*1) + "<br>");
+document.write (table + "x2=" + (table*2) + "<br>");
+document.write (table + "x3=" + (table*3) + "<br>");
+document.write (table + "x4=" + (table*4) + "<br>");
+document.write (table + "x5=" + (table*5) + "<br>");
+document.write (table + "x6=" + (table*6) + "<br>");
+document.write (table + "x7=" + (table*7) + "<br>");
+document.write (table + "x8=" + (table*8) + "<br>");
+document.write (table + "x9=" + (table*9) + "<br>");
+document.write (table + "x10=" + (table*10) + "<br><br>");
+
+//Question 6
 
 
 
